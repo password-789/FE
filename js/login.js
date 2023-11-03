66fcc8184f337ece41a7178bbaba13e90effcb56
SJ-Project <etiusujin07@naver.com>
2023-11-03 09:34:24 +0900

    feat : login 기능 추가

---
+++ js/login.js
@@ -0,0 +1,30 @@
+function login(){
+    var id=document.querySelector('#id');
+    var pw = document.querySelector('#pw');
+
+    if(id.value =="" || pw.value ==""){
+        alert("로그인을 할 수 없습니다.")
+    }else{
+        location.href = 'index.html';
+    }
+}
+
+function back(){
+    history.go(-1);
+}
+function create_id(){
+    var id = document.querySelector('#id');
+    var pw = document.querySelector('#pw');
+    var r_id = document.querySelector('r_pw');
+
+    if(id.value == "" || pw.value == "" || r_pw.value ==""){
+        alert("회원가입을 할 수 없습니다.")
+    }else{
+        if(pw.value !== r_pw.value){
+            alert("비밀번호를 확인해주세요.")
+        }
+        else{
+            location.href = 'login.html'
+        }
+    }
+}
